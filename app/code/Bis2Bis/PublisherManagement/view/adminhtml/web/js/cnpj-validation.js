358ac46eb2f4150fdb4543805b4b09c60d8224fc
Gabriel dos Anjos <gabriel.peixinho@uello.com.br>
2023-08-07 10:22:45 -0300

    removed console.log from cnpj validation

--- app/code/Bis2Bis/PublisherManagement/view/adminhtml/web/js/cnpj-validation.js
+++ app/code/Bis2Bis/PublisherManagement/view/adminhtml/web/js/cnpj-validation.js
@@ -7,7 +7,6 @@ require(
         validator.addRule(
             'cnpj-validation',
             function (value) {
-                console.log((/^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$/.test(value)));
                 return (/^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$/.test(value));
             },
             $.mage.__('CNPJ field invalid.')
